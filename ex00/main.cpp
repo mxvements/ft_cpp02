c7a9c29eb0a7cdc928eaecfe38a14e2160f22a86
luciama2 <luciama2@student.42madrid.com>
2024-10-10 21:19:56 +0200

    fix: added main output as comment

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,31 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 20:30:31 by luciama2          #+#    #+#             */
-/*   Updated: 2024/10/10 21:06:16 by luciama2         ###   ########.fr       */
+/*   Updated: 2024/10/10 21:19:38 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+/**
+ * $> ./a.out 
+ * Default constructor called
+ * Copy constructor called
+ * Copy assignment operator called // <-- This line may be missing depending on your implementation
+ * getRawBits member function called
+ * Default constructor called
+ * Copy assignment operator called
+ * getRawBits member function called
+ * getRawBits member function called
+ * 0
+ * getRawBits member function called
+ * 0
+ * getRawBits member function called
+ * 0
+ * Destructor called
+ * Destructor called
+ * Destruct
+ */
 int main( void )
 {
 	Fixed a;
